8b2b2f8e2c1f4182795f85a2ce0f3be87bc7af85
aniszad <91326965+aniszad@users.noreply.github.com>
2024-01-28 00:21:56 +0100

    original

--- venv/src/TaskyApi/projects/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Project(models.Model):
-    title = models.CharField(max_length = 99)
-    description = models.CharField(max_length = 500)
-    priority = models.CharField(max_length=6)
-    owner_id = models.IntegerField()
